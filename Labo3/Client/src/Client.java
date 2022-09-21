7019d7193edd1f740ca025ebaf6513634788484e
Yannick Gilbert <yannickgilbert1234567890@outlook.com>
2022-09-20 21:32:06 -0400

    presque final commit

--- Labo3/Client/src/Client.java
+++ Labo3/Client/src/Client.java
@@ -8,6 +8,8 @@ public class Client
     private DataInputStream in = null;
     private DataOutputStream out = null;
 
+    private String m_Protocol = "Yan";
+
     public Client(String address, int port)
     {
         try
@@ -17,20 +19,27 @@ public class Client
             System.out.println("Connecter");
 
             in = new DataInputStream(System.in);
+            DataInputStream inSocket = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
             String line = "";
 
-            while(!line.equals("Fini"))
+            while(!line.equals("Yan close"))
             {
                 try
                 {
                     line = in.readLine();
+                    //ajoute le protole a la commande entré
+                    line = m_Protocol + " " + line;
                     out.writeUTF(line);
+
+                    CmdSend(line, inSocket);
+
                 }
                 catch (IOException i)
                 {
                     System.out.println(i);
+                    break;
                 }
             }
             try
@@ -47,6 +56,44 @@ public class Client
         catch (Exception e)
         {
             System.out.println(e);
+        }
+    }
+
+    void CmdSend(String cmd, DataInputStream in)
+    {
+        String commande = "";
+        for(int i = 0; i < cmd.length(); i++)
+        {
+            if(cmd.charAt(i) == ':')
+            {
+                break;
+            }
+            commande += cmd.charAt(i);
+        }
+
+        switch (commande)
+        {
+            case "Yan getAllFolder":
+                PrintUTF(in);
+                break;
+            case "Yan read":
+                PrintUTF(in);
+                break;
+            default:
+                //default case
+        }
+    }
+
+    void PrintUTF(DataInputStream in)
+    {
+        try
+        {
+            String test = in.readUTF();
+            System.out.println(test);
+        }
+        catch (IOException i)
+        {
+            System.out.println(i);
         }
     }
 }
--- Labo3/Serveur/src/ServeurClient/Serveur.java
+++ Labo3/Serveur/src/ServeurClient/Serveur.java
@@ -1,6 +1,7 @@
 package ServeurClient;
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Serveur
 {
@@ -11,6 +12,9 @@ public class Serveur
     //veux ce connecter a la connection c'est aussi lui qui va accepter si quelqun demande de ce connecter
     private ServerSocket server = null;
     private DataInputStream in = null;
+    private DataOutputStream out = null;
+
+    private String m_Protocol = "Yan";
 
     public Serveur(int port)
     {
@@ -27,20 +31,23 @@ public class Serveur
 
             //permet d'aller voir ce que le socket recois
             in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
             String line = "";
 
-            while(!line.equals("Fini"))
+            String endCondition = m_Protocol + " close";
+            while(!line.equals(endCondition))
             {
                 try
                 {
                     //retourn le output du client
                     line = in.readUTF();
-                    System.out.println(line);
+                    CmdReceive(line, out);
                 }
                 catch (IOException i)
                 {
                     System.out.println(i);
+                    break;
                 }
             }
             System.out.println("Connection Fermer");
@@ -51,6 +58,162 @@ public class Serveur
         catch (IOException a)
         {
             System.out.println(a);
+        }
+    }
+
+    private void CmdReceive(String cmd, DataOutputStream out)
+    {
+        System.out.println(cmd);
+        if(CheckProtocole(cmd))
+        {
+            String commande = "";
+            int i;
+            for(i = 0; i < cmd.length(); i++)
+            {
+                if(cmd.charAt(i) == ':')
+                {
+                    i++;
+                    break;
+                }
+                commande += cmd.charAt(i);
+            }
+
+            String fileName = "";
+            for(;i < cmd.length(); i++)
+            {
+                if(cmd.charAt(i) == ':')
+                {
+                    i++;
+                    break;
+                }
+                fileName += cmd.charAt(i);
+            }
+
+            switch (commande)
+            {
+                //cree un fichier
+                case "Yan createFile":
+                    CreateFile(fileName);
+                    break;
+                case "Yan getAllFolder":
+                        GetAllFolder(out);
+                    break;
+                case "Yan write":
+                    String text = "";
+                    for(;i < cmd.length(); i++)
+                    {
+                        text += cmd.charAt(i);
+                    }
+                    WriteFile(fileName, text);
+                    break;
+                case "Yan read":
+                    ReadFile(fileName, out);
+                    break;
+                default:
+                    //default case
+            }
+        }
+    }
+
+    private boolean CheckProtocole(String cmd)
+    {
+        if(cmd.length() >= m_Protocol.length())
+        {
+            String protocol = "";
+            for(int i = 0; i < m_Protocol.length(); i++)
+            {
+                protocol += cmd.charAt(i);
+            }
+            if(protocol.equals(m_Protocol))
+            {
+                System.out.println("Protocol is good");
+                return true;
+            }
+        }
+        System.out.println("Protocol is not good");
+        return false;
+    }
+
+    private void CreateFile(String fileName)
+    {
+        File newFile = new File("FileCreate\\" + fileName);
+        try
+        {
+            if(newFile.createNewFile())
+            {
+                System.out.println("File created: " + newFile.getName());
+            }
+            else
+            {
+                System.out.println("File already exists.");
+            }
+        }
+        catch (IOException a)
+        {
+            System.out.println(a);
+        }
+    }
+
+    private void GetAllFolder(DataOutputStream out)
+    {
+        try
+        {
+            File file = new File("FileCreate");
+            String[] tabl = file.list();
+
+            String text = "";
+
+            for(int i = 0; i < tabl.length; i++)
+            {
+                text += tabl[i];
+                if(i != tabl.length - 1)
+                {
+                    text += '\n';
+                }
+            }
+
+            out.writeUTF(text);
+        }
+        catch (IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    private void WriteFile(String fileName, String text)
+    {
+        System.out.println("ca peut etre marcher");
+        try
+        {
+            FileWriter writer = new FileWriter("FileCreate\\" + fileName);
+            writer.write(text);
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        }
+        catch (IOException a)
+        {
+            System.out.println(a);
+        }
+    }
+
+    private void ReadFile(String fileName, DataOutputStream out)
+    {
+        try
+        {
+            File file = new File("FileCreate\\" + fileName);
+            Scanner reader = new Scanner(file);
+
+            String text = "";
+
+            while (reader.hasNextLine())
+            {
+                text += reader.nextLine();
+            }
+            out.writeUTF(text);
+        }
+        catch (IOException i)
+        {
+            System.out.println(i);
         }
     }
 }
